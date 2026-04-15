345f92a80fb9c6712c745cc212d97aaa2d8c1947
kroniak <me@kroniak.net>
2026-04-15 15:46:01 +0300

    add(i18n): main internationalization

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import common from "@messages/common.json";
 import React from "react";
 import { ErrorComponent } from "@components/errors/error-component";
+import { useTranslations } from "next-intl";
 
 export default function GlobalErrorPage({ error }: { error: Error & { digest?: string } }) {
+  const t = useTranslations("common");
+
   return (
     <ErrorComponent
-      title={common.errors.globalError.title}
-      subTitle={common.errors.globalError.subTitle}
-      description={common.errors.globalError.description}
+      title={t("errors.globalError.title")}
+      subTitle={t("errors.globalError.subTitle")}
+      description={t("errors.globalError.description")}
       error={error}
     />
   );
